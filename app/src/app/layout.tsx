601bf9e1048a857e3ca88d2f1bad392692992554
Nicholas Costa <nicholascostadev@gmail.com>
2024-03-15 16:11:20 -0300

    feat: init shadcn-ui

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Hackathon Solana",
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("bg-background min-h-screen font-sans antialiased", fontSans.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
