4eea637d95819b44565ec8c63956cde695d5797d
Bernardo Muller <bernardo.muller.bm@gmail.com>
2024-03-20 15:54:23 -0300

    refactor: now we really fix the problem with the i18n lib

--- app/src/app/[locale]/investments/page.tsx
+++ app/src/app/[locale]/investments/page.tsx
@@ -1,10 +1,7 @@
-import { PageProps } from "@/lib/types";
 import { InvestmentsPageContent } from "./_components/content";
 import { InvestmentsPageHeader } from "./_components/header";
-import { unstable_setRequestLocale } from "next-intl/server";
 
-export default function InvestmentsPage({ params }: PageProps<{ locale: string }>) {
-  unstable_setRequestLocale(params.locale);
+export default function InvestmentsPage() {
   return (
     <div className="min-h-[calc(100vh-153px)] dark:bg-zinc-100">
       <InvestmentsPageHeader />
