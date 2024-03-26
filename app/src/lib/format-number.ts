b486907261502efeae14690ef65ef0f4a1d8ba4e
Thalles Passos <contato@thalles.me>
2024-03-26 15:46:18 -0300

    feat: list offers in frontend

--- app/src/lib/format-number.ts
+++ app/src/lib/format-number.ts
@@ -1,6 +1,15 @@
+import BN from "bn.js";
+
 export function formatNumber(value: number) {
   return new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
   }).format(value);
+}
+
+export function formatBigNumber(value: BN) {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value.toNumber()); // TODO: this is not right, it'll overflow
 }
