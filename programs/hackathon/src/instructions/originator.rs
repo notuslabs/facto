9d30c217dd5789ea0d3e22b16432573f10f674a0
Thalles Passos <contato@thalles.me>
2024-03-27 15:27:54 -0300

    feat: add originator validations

--- programs/hackathon/src/instructions/originator.rs
+++ programs/hackathon/src/instructions/originator.rs
@@ -7,14 +7,18 @@ pub fn create_originator(
     description: String,
     token_slug: String,
 ) -> Result<()> {
-    require!(name.len() > 30, ErrorCode::MaxNameLengthExceeded);
+    require!(name.len() < 30, ValidationError::MaxNameLengthExceeded);
     require!(
-        description.len() > 500,
-        ErrorCode::MaxDescriptionLengthExceeded
+        description.len() < 500,
+        ValidationError::MaxDescriptionLengthExceeded
     );
-    require!(token_slug.len() > 30, ErrorCode::MaxTokenSlugLengthExceeded);
+    require!(
+        token_slug.len() < 30,
+        ValidationError::MaxTokenSlugLengthExceeded
+    );
 
     let originator = &mut ctx.accounts.originator;
+
     originator.name = name;
     originator.description = description;
     originator.total_offers = 0;
@@ -30,6 +34,11 @@ pub fn edit_originator(
     name: String,
     description: String,
 ) -> Result<()> {
+    require!(name.len() < 30, ValidationError::MaxNameLengthExceeded);
+    require!(
+        description.len() < 500,
+        ValidationError::MaxDescriptionLengthExceeded
+    );
     let originator = &mut ctx.accounts.originator;
     originator.name = name;
     originator.description = description;
@@ -37,7 +46,7 @@ pub fn edit_originator(
 }
 
 #[error_code]
-pub enum ErrorCode {
+pub enum ValidationError {
     #[msg("Max name length exceeded. Maximum length is 30")]
     MaxNameLengthExceeded,
     #[msg("Max description length exceeded. Maximum length is 500")]
