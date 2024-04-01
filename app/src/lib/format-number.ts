02849a5fa28e93c11d9d43b8019a75e88959757e
Nicholas Costa <nicholascostadev@gmail.com>
2024-04-01 18:50:37 -0300

    refactor: change default currency to USD

--- app/src/lib/format-number.ts
+++ app/src/lib/format-number.ts
@@ -1,13 +1,13 @@
 import BN from "bn.js";
 
-export function formatNumber(value: number, locale = "pt-BR", currency = "BRL") {
+export function formatNumber(value: number, locale = "pt-BR", currency = "USD") {
   return new Intl.NumberFormat(locale, {
     style: "currency",
     currency: currency,
   }).format(value);
 }
 
-export function formatBigNumber(value: BN, locale = "pt-BR", currency = "BRL") {
+export function formatBigNumber(value: BN, locale = "pt-BR", currency = "USD") {
   return new Intl.NumberFormat(locale, {
     style: "currency",
     currency: currency,
