d5d04685e0ebd877545e7e81f9c03ac090422a9e
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2024-04-01 16:28:02 -0300

    feat: add README

--- app/src/lib/idl/facto-idl-types.ts
+++ app/src/lib/idl/facto-idl-types.ts
@@ -1250,6 +1250,10 @@ export type Hackathon = {
         "kind": "struct",
         "fields": [
           {
+            "name": "originator",
+            "type": "publicKey"
+          },
+          {
             "name": "id",
             "type": "string"
           },
@@ -1276,10 +1280,6 @@ export type Hackathon = {
           {
             "name": "acquiredAmount",
             "type": "u64"
-          },
-          {
-            "name": "originator",
-            "type": "publicKey"
           },
           {
             "name": "installmentsCount",
@@ -2700,6 +2700,10 @@ export const IDL: Hackathon = {
         "kind": "struct",
         "fields": [
           {
+            "name": "originator",
+            "type": "publicKey"
+          },
+          {
             "name": "id",
             "type": "string"
           },
@@ -2726,10 +2730,6 @@ export const IDL: Hackathon = {
           {
             "name": "acquiredAmount",
             "type": "u64"
-          },
-          {
-            "name": "originator",
-            "type": "publicKey"
           },
           {
             "name": "installmentsCount",
