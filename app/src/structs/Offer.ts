89fc2e6ebcf56da31d2d656fae9146c814b4a56d
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2024-04-05 10:31:52 -0300

    refactor: update status

--- app/src/structs/Offer.ts
+++ app/src/structs/Offer.ts
@@ -114,7 +114,7 @@ export class Offer {
     }
 
     if (this.acquiredAmount == this.goalAmount) {
-      if (currentTime < deadlineTime) {
+      if (currentTime < deadlineTime && this.totalInstallmentsPaid == 0) {
         return OfferStatus.Funded;
       }
 
